3390cc5e94b727dc4fffb7dddb76fb73774e6748
tmich1997 <nu842639@gmail.com>
2023-10-19 13:32:47 +1100

    first commit

--- Case Study #1 - Danny's Diner/Week_1.sql
+++ Case Study #1 - Danny's Diner/Week_1.sql
@@ -143,6 +143,3 @@ GROUP by customer_id;
 -- 10. In the first week after a customer joins the program (including their join date) they 
 -- earn 2x points on all items, not just sushi - 
 -- how many points do customer A and B have at the end of January?
-
-
--- hello 
