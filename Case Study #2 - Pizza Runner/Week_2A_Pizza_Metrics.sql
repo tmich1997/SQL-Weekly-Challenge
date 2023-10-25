04c1b0f16f708e6e2c1ee931d2d17b4a52a8008e
tmich1997 <142772458+tmich1997@users.noreply.github.com>
2023-10-25 16:02:44 +1100

    Add files via upload

--- Case Study #2 - Pizza Runner/Week_2A_Pizza_Metrics.sql
+++ Case Study #2 - Pizza Runner/Week_2A_Pizza_Metrics.sql
@@ -9,3 +9,9 @@ SELECT
 from PizzaRunner..customer_orders;
 
 -- 3. How many successful orders were delivered by each runner?
+SELECT
+    ro.runner_id,
+    COUNT(ro.runner_id)
+from PizzaRunner..runner_orders ro
+where ro.distance != 'null'
+GROUP BY ro.runner_id;
