e2ff8432faabb191a15860f029623b523eba528a
tmich1997 <142772458+tmich1997@users.noreply.github.com>
2023-11-10 12:43:00 +1100

    Adding a .md file

---
+++ Case Study #2 - Pizza Runner/A. Pizza Metrics.md
@@ -0,0 +1,11 @@
+# 🍜 Case Study #1: Danny's Diner
+
+This is the beginning of my 8-Weeks of SQL challenge journey. I had a lot of fun and learned a lot expecially about CTE (common table expression), window fucntions, partition, order just to name a few.
+
+If you feel inspired to learn SQL or want to challenge yourself you can find the challenges, [here](https://8weeksqlchallenge.com/case-study-1/).
+
+## Initial Set-up
+I am using VS Code as my IDE for this, and I used a SQL Server extension (downloaded from VS Code extensions) which will be used to then connect to the SQL server where you can then create the necessary tables for the task. To create the tables, you can just copy the CREATE command from the link provided earlier.
+
+## The Task
+Danny wants to u
--- Case Study #2 - Pizza Runner/Week_2A_Pizza_Metrics.sql
+++ Case Study #2 - Pizza Runner/Week_2A_Pizza_Metrics.sql
@@ -1,81 +1,133 @@
--- Cleaning, Part 1: Cleaning the "customer_orders" table
-DROP TABLE IF EXISTS clean_customer_orders;
-
-CREATE TABLE clean_customer_orders (
-    "order_id" INT,
-    "customer_id" INT,
-    "pizza_id" INT,
-    "exclusions" VARCHAR(4),
-    "extras" VARCHAR(4),
-    "order_time" DATETIME
-);
-
-INSERT INTO clean_customer_orders 
-    ("order_id", "customer_id", "pizza_id", "exclusions", "extras", "order_time")
-SELECT
-    co.order_id,
-    co.customer_id,
-    co.pizza_id,
-    (CASE
-        when
-            co.exclusions = '' or co.exclusions = 'null'
-        then
-            null
-        else
-            co.exclusions
-    end) as exclusions,
-    (CASE
-        when
-            co.extras = 'null' or co.extras = ''
-        then
-            null
-        else
-            co.extras
-    end) as extras,
-    co.order_time
-from PizzaRunner..customer_orders co;
-
-SELECT *
-from clean_customer_orders;
-
--- Cleaning, Part 2: Cleaning the "runner_orders" table
-DROP TABLE IF EXISTS clean_runner_orders;
-
-CREATE TABLE clean_runner_orders (
-  "order_id" INTEGER,
-  "runner_id" INTEGER,
-  "pickup_time" VARCHAR(19),
-  "distance" VARCHAR(7),
-  "duration" VARCHAR(10),
-  "cancellation" VARCHAR(23)
-);
-
-
 -- 1. How many pizzas were ordered?
 SELECT
     count(order_id) as order_count
-from PizzaRunner..customer_orders;
+from PizzaRunner..customer_orders
+;
 
 -- 2. How many unique customer orders were made?
 SELECT
     COUNT(distinct order_id) as distinct_order
-from PizzaRunner..customer_orders;
+from PizzaRunner..customer_orders
+;
 
 -- 3. How many successful orders were delivered by each runner?
 SELECT
     ro.runner_id,
     COUNT(ro.runner_id) as successful_orders
 from PizzaRunner..runner_orders ro
-where ro.distance != 'null'
-GROUP BY ro.runner_id;
+where 
+    ro.distance != 'null'
+GROUP BY 
+    ro.runner_id
+;
 
 -- 4. How many of each type of pizza was delivered?
 SELECT
+    pn.pizza_name as Pizza_type,
+    COUNT(*) as #_of_pizzas_deliveries
+FROM clean_runner_orders rc
+JOIN customer_orders co
+    ON rc.order_id = co.order_id
+JOIN pizza_names pn
+    on co.pizza_id = pn.pizza_id
+WHERE 
+    cancellation IS NULL
+GROUP BY 
+    pn.pizza_name
+;
+
+-- 5. How many Vegetarian and Meatlovers were ordered by each customer?
+SELECT
+    cco.customer_id,
     pn.pizza_name,
-    COUNT(co.pizza_id)
-from PizzaRunner..customer_orders co
-join PizzaRunner..pizza_names pn
-    on pn.pizza_id = co.pizza_id
-join PizzaRunner..runner_orders ro
-    on ro.order_id = co.order_id
-group BY pn.pizza_name;
+    COUNT(cco.pizza_id) as number_of_pizzas_ordered
+from clean_customer_orders cco
+JOIN pizza_names pn
+    ON cco.pizza_id = pn.pizza_id
+GROUP BY
+    cco.customer_id,
+    pn.pizza_name
+order BY 
+    cco.customer_id
+;
+
+-- 6. What was the maximum number of pizzas delivered in a single order?
+SELECT
+    top 1
+    cco.customer_id,
+    cco.order_id,
+    COUNT(cco.order_id) pizza_count
+from clean_customer_orders cco
+GROUP BY
+    cco.customer_id,
+    cco.order_id
+ORDER BY
+    COUNT(cco.order_id) desc
+;
+
+-- 7. For each customer, how many delivered pizzas had at least 1 
+-- change and how many had no changes?
+-- 1 change: either EXCLUSIONS or EXTRAS is NOT NULL
+-- no change: both EXTRAS and EXCLUSIONS is NULL
+SELECT
+    cco.customer_id,
+    sum(
+        CASE
+            WHEN cco.extras is null and cco.exclusions is null then 1
+            else 0
+        END
+    ) as no_change_in_pizza,
+    sum(
+        CASE
+            WHEN cco.extras is not null or cco.exclusions is not null then 1
+            else 0
+        END
+    ) as change_in_pizza
+from clean_customer_orders cco
+JOIN clean_runner_orders cro
+    on cro.order_id = cco.order_id
+WHERE
+    cro.cancellation is null
+GROUP BY
+    cco.customer_id
+ORDER BY
+    cco.customer_id
+;
+
+-- 8. How many pizzas were delivered that had both exclusions and extras?
+SELECT
+    cco.customer_id,
+    SUM(
+        CASE
+            WHEN cco.extras is not null and cco.exclusions is not null then 1
+            else 0
+        END
+    ) as no_change_in_pizza
+from clean_customer_orders cco
+JOIN clean_runner_orders cro
+    on cro.order_id = cco.order_id
+WHERE
+    cro.cancellation is null
+GROUP BY
+    cco.customer_id
+ORDER BY
+    cco.customer_id
+;
+
+-- 9. What was the total volume of pizzas ordered for each hour of the day?
+SELECT
+    count(cco.order_id) as pizza_count,
+    datepart(hour, cco.order_time) as hour_of_day
+from clean_customer_orders cco
+GROUP BY
+    datepart(hour, cco.order_time)
+;
+
+-- 10. What was the volume of orders for each day of the week?
+SELECT
+    count(cco.order_id) as pizza_count,
+    DATENAME(DW, cco.order_time) as day_of_week
+from clean_customer_orders cco
+GROUP BY
+    DATENAME(DW, cco.order_time)
+;
